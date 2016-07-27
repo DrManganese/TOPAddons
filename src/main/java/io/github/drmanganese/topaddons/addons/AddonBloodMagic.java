58c99b1187b6b540b91e1fc5fc1c88d5e4a86057
DrManganese <music.mathias@gmail.com>
2016-07-27 17:07:59 +0200

    Blood Magic: Node filter element

--- src/main/java/io/github/drmanganese/topaddons/addons/AddonBloodMagic.java
+++ src/main/java/io/github/drmanganese/topaddons/addons/AddonBloodMagic.java
@@ -2,13 +2,23 @@ package io.github.drmanganese.topaddons.addons;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import io.github.drmanganese.topaddons.Config;
+import io.github.drmanganese.topaddons.TOPRegistrar;
 import io.github.drmanganese.topaddons.api.TOPAddon;
+import io.github.drmanganese.topaddons.elements.bloodmagic.ElementNodeFilter;
 
 import WayofTime.bloodmagic.api.altar.IBloodAltar;
 import WayofTime.bloodmagic.block.BlockLifeEssence;
+import WayofTime.bloodmagic.item.sigil.ItemSigilDivination;
+import WayofTime.bloodmagic.routing.IMasterRoutingNode;
+import WayofTime.bloodmagic.tile.routing.TileFilteredRoutingNode;
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
 import mcjty.theoneprobe.api.ProbeMode;
@@ -16,15 +26,56 @@ import mcjty.theoneprobe.api.ProbeMode;
 @TOPAddon(dependency = "BloodMagic")
 public class AddonBloodMagic extends AddonBlank {
 
+    public static int ELEMENT_NODE_FILTER;
+
+    @Override
+    public void registerElements() {
+        ELEMENT_NODE_FILTER = TOPRegistrar.GetTheOneProbe.probe.registerElementFactory(ElementNodeFilter::new);
+    }
+
     @Override
     public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
         TileEntity tile = world.getTileEntity(data.getPos());
+        boolean holdingSigil = isDivinationSigil(player.getHeldItem(EnumHand.MAIN_HAND)) || isDivinationSigil(player.getHeldItem(EnumHand.OFF_HAND));
         if (tile != null) {
-            if (tile instanceof IBloodAltar) {
+            if (tile instanceof IBloodAltar && (!Config.BloodMagic.requireSigil || holdingSigil)) {
                 IBloodAltar altar = (IBloodAltar) tile;
                 textPrefixed(probeInfo, "Tier", altar.getTier().toInt() + "", TextFormatting.RED);
                 AddonForge.addTankElement(probeInfo, "Blood Altar", "Life Essence", altar.getCurrentBlood(), altar.getCapacity(), BlockLifeEssence.getLifeEssence().getColor(), mode);
             }
+
+            if (tile instanceof TileFilteredRoutingNode && !(tile instanceof IMasterRoutingNode)) {
+                TileFilteredRoutingNode node = (TileFilteredRoutingNode) tile;
+                ItemStack filterStack = node.getFilterStack(data.getSideHit());
+                if (filterStack != null) {
+//                    ItemInventory filterInv = new ItemInventory(filterStack, 9, "");
+//                    probeInfo.horizontal(new LayoutStyle().spacing(-4))
+//                            .item(filterStack)
+//                            .item(filterInv.getStackInSlot(0))
+//                            .item(filterInv.getStackInSlot(1))
+//                            .item(filterInv.getStackInSlot(2))
+//                            .item(filterInv.getStackInSlot(3))
+//                            .item(filterInv.getStackInSlot(4))
+//                            .item(filterInv.getStackInSlot(5))
+//                            .item(filterInv.getStackInSlot(6))
+//                            .item(filterInv.getStackInSlot(7))
+//                            .item(filterInv.getStackInSlot(8));
+                    BlockPos sidePos = data.getPos().offset(data.getSideHit());
+                    if (world.getTileEntity(sidePos) != null) {
+                        IBlockState sideState = world.getBlockState(sidePos);
+                        ItemStack inventoryOnSide = sideState.getBlock().getPickBlock(sideState, new RayTraceResult(data.getHitVec(), data.getSideHit().getOpposite(), sidePos), world, sidePos, player);
+                        addFilterElement(probeInfo, data.getSideHit().getName(), inventoryOnSide, filterStack);
+                    }
+                }
+            }
         }
+    }
+
+    private void addFilterElement(IProbeInfo probeInfo, String side, ItemStack inventoryOnSide, ItemStack filterStack) {
+        probeInfo.element(new ElementNodeFilter(side, inventoryOnSide, filterStack));
+    }
+
+    private boolean isDivinationSigil(ItemStack heldStack) {
+        return heldStack != null && heldStack.getItem() instanceof ItemSigilDivination;
     }
 }
---
+++ src/main/java/io/github/drmanganese/topaddons/elements/bloodmagic/ElementNodeFilter.java
@@ -0,0 +1,70 @@
+package io.github.drmanganese.topaddons.elements.bloodmagic;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+import io.github.drmanganese.topaddons.addons.AddonBloodMagic;
+
+import WayofTime.bloodmagic.item.inventory.ItemInventory;
+import io.netty.buffer.ByteBuf;
+import mcjty.theoneprobe.api.IElement;
+import mcjty.theoneprobe.network.NetworkTools;
+import mcjty.theoneprobe.rendering.RenderHelper;
+
+public class ElementNodeFilter implements IElement {
+
+    private final String side;
+    private final ItemStack inventoryOnSide;
+    private final ItemStack filterStack;
+
+    public ElementNodeFilter(String side, ItemStack inventoryOnSide, ItemStack filterStack) {
+        this.side = side;
+        this.inventoryOnSide = inventoryOnSide;
+        this.filterStack = filterStack;
+    }
+
+    public ElementNodeFilter(ByteBuf buf) {
+        this.side = NetworkTools.readString(buf);
+        this.inventoryOnSide = NetworkTools.readItemStack(buf);
+        this.filterStack = NetworkTools.readItemStack(buf);
+    }
+
+    @Override
+    public void render(int x, int y) {
+        Minecraft mc = Minecraft.getMinecraft();
+        RenderHelper.drawBeveledBox(x, y, x + 18, y + 18, 0xff969696, 0xff969696, 0x44969696);
+        RenderHelper.renderItemStack(mc, mc.getRenderItem(), inventoryOnSide, x + 1, y + 1, "");
+        RenderHelper.renderItemStack(mc, mc.getRenderItem(), filterStack, x + 18, y + 1, "");
+        RenderHelper.drawBeveledBox(x + 34, y, x + 180, y + 18, 0xff969696, 0xff969696, 0x44969696);
+
+        ItemInventory filterInv = new ItemInventory(filterStack, 9, "");
+        int xOffset = 0;
+        for (int i = 0; i < 9; i++) {
+            if (filterInv.getStackInSlot(i) != null) {
+                RenderHelper.renderItemStack(mc, mc.getRenderItem(), filterInv.getStackInSlot(i), x + 35 + xOffset, y + 1, "");
+                xOffset += 16;
+            }
+        }
+    }
+
+    @Override
+    public int getWidth() {
+        return 100;
+    }
+
+    @Override
+    public int getHeight() {
+        return 18;
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        NetworkTools.writeString(buf, side);
+        NetworkTools.writeItemStack(buf, inventoryOnSide);
+        NetworkTools.writeItemStack(buf, filterStack);
+    }
+
+    @Override
+    public int getID() {
+        return AddonBloodMagic.ELEMENT_NODE_FILTER;
+    }
+}
