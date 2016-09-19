01b38c5983fc17918c373e41137a57635ae4c2f5
DrManganese <music.mathias@gmail.com>
2016-09-19 10:28:39 +0200

    Fixed server crash because of wrong SideOnly annotation

--- src/main/java/io/github/drmanganese/topaddons/reference/EnumChip.java
+++ src/main/java/io/github/drmanganese/topaddons/reference/EnumChip.java
@@ -5,7 +5,6 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public enum EnumChip {
     STANDARD("chip", -0.625F, -1.84F, -0.3F, 1.25F, 1F/32F),
     SPECTACLES("spectacles", -.1935F, -1.719F, -.315F, .62F, 1F/64F),
@@ -14,7 +13,9 @@ public enum EnumChip {
     private final String texture;
     private final float xTranslate, yTranslate, zTranslate;
     private final float scale, thickness;
-    private TextureAtlasSprite sprite;
+
+    @SideOnly(Side.CLIENT)
+    TextureAtlasSprite sprite;
 
 
     EnumChip(String texture, float xTranslate, float yTranslate, float zTranslate, float scale, float thickness) {
@@ -30,10 +31,12 @@ public enum EnumChip {
         return texture;
     }
 
+    @SideOnly(Side.CLIENT)
     public TextureAtlasSprite getSprite() {
         return sprite;
     }
 
+    @SideOnly(Side.CLIENT)
     public void setSprite(TextureAtlasSprite sprite) {
         this.sprite = sprite;
     }
