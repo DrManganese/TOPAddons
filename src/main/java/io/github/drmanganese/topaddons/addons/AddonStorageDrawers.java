8a54dfd3dec43d6486aadc2ec164d9ddd5cfae20
DrManganese <music.mathias@gmail.com>
2016-10-25 19:56:51 +0200

    Border for extended view

--- src/main/java/io/github/drmanganese/topaddons/addons/AddonStorageDrawers.java
+++ src/main/java/io/github/drmanganese/topaddons/addons/AddonStorageDrawers.java
@@ -10,11 +10,15 @@ import io.github.drmanganese.topaddons.api.TOPAddon;
 
 import com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityDrawers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import mcjty.theoneprobe.api.ElementAlignment;
 import mcjty.theoneprobe.api.IProbeConfig;
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
 import mcjty.theoneprobe.api.ProbeMode;
+import mcjty.theoneprobe.config.Config;
 
 @TOPAddon(dependency = "StorageDrawers")
 public class AddonStorageDrawers extends AddonBlank {
@@ -29,38 +33,47 @@ public class AddonStorageDrawers extends AddonBlank {
                 return;
             }
 
+
             if (mode == ProbeMode.EXTENDED) {
+                List<ItemStack> stacks = new ArrayList<>();
                 for (int i = 0; i < tile.getDrawerCount(); i++) {
                     ItemStack stack = tile.getDrawer(i).getStoredItemCopy();
                     if (stack != null) {
+                        stacks.add(stack);
+                    }
+                }
+
+                if (!stacks.isEmpty()) {
+                    IProbeInfo vertical = probeInfo.vertical(probeInfo.defaultLayoutStyle().borderColor(Config.chestContentsBorderColor).spacing(0));
+                    for (ItemStack stack : stacks) {
                         if (tile.isVending()) {
                             ItemStack infiStack = ItemStack.copyItemStack(stack);
                             infiStack.stackSize = 1;
-                            probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
+                            vertical.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
                                     .item(infiStack)
                                     .vertical(probeInfo.defaultLayoutStyle().spacing(0))
                                     .text(stack.getDisplayName())
                                     .text(TextFormatting.GRAY + "[\u221e]");
-                        } else {
 
+                        } else {
                             int r = stack.stackSize % 64;
                             int q = (stack.stackSize - r) / 64;
-                            probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
-                                    .item(stack)
+                            vertical.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)).item(stack)
                                     .vertical(probeInfo.defaultLayoutStyle().spacing(0))
                                     .text(stack.getDisplayName())
                                     .text(TextFormatting.GRAY + "[" + (stack.stackSize >= 64 ? q + "x64 + " : "") + r + "]");
                         }
                     }
-                }
+            }
 
-                textPrefixed(probeInfo, "Stack limit", tile.isUnlimited() ? "\u221e" : tile.getDrawerCapacity() + " (x" + tile.getEffectiveStorageMultiplier() + ")");
-                if (tile.getOwner() != null && tile.getOwner().compareTo(player.getUniqueID()) != 0) {
-                    probeInfo.text(TextFormatting.RED + TextFormatting.ITALIC.toString() + "Protected");
-                }
+            textPrefixed(probeInfo, "Stack limit", tile.isUnlimited() ? "\u221e" : tile.getDrawerCapacity() * tile.getEffectiveStorageMultiplier() + " (x" + tile.getEffectiveStorageMultiplier() + ")", TextFormatting.AQUA);
+            if (tile.getOwner() != null && tile.getOwner().compareTo(player.getUniqueID()) != 0) {
+                probeInfo.text(TextFormatting.RED + TextFormatting.ITALIC.toString() + "Protected");
             }
         }
     }
+
+}
 
     @Override
     public void getProbeConfig(IProbeConfig config, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
