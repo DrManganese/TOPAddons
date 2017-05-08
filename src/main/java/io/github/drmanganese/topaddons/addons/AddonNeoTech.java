d6f2398d4bb29d98c190b782fedfb56998461908
DrManganese <music.mathias@gmail.com>
2017-05-08 20:39:48 +0200

    Updated neotech support, no more machine progress, sapling info and crusher.

--- src/main/java/io/github/drmanganese/topaddons/addons/AddonNeoTech.java
+++ src/main/java/io/github/drmanganese/topaddons/addons/AddonNeoTech.java
@@ -1,20 +1,16 @@
 package io.github.drmanganese.topaddons.addons;
 
-import net.minecraft.block.BlockBasePressurePlate;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 import io.github.drmanganese.topaddons.api.TOPAddon;
-import io.github.drmanganese.topaddons.styles.ProgressStyleTOPAddonGrey;
 
 import com.teambrmodding.neotech.common.tiles.MachineGenerator;
-import com.teambrmodding.neotech.common.tiles.MachineProcessor;
-import com.teambrmodding.neotech.common.tiles.machines.TileGrinder;
 import com.teambrmodding.neotech.common.tiles.machines.operators.TileTreeFarm;
+import com.teambrmodding.neotech.common.tiles.traits.IUpgradeItem;
 
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
@@ -27,40 +23,20 @@ public class AddonNeoTech extends AddonBlank {
     public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data) {
         TileEntity tile = world.getTileEntity(data.getPos());
         if (tile != null) {
-            if (tile instanceof MachineProcessor && ((MachineProcessor) tile).canProcess()) {
-                probeInfo.progress(100 * ((MachineProcessor) tile).cookTime() / ((MachineProcessor) tile).getCookTime(), 100, new ProgressStyleTOPAddonGrey().filledColor(0xff111111).alternateFilledColor(0xff222222).suffix("%").prefix("Progress: "));
-            }
-
-            if (tile instanceof TileGrinder) {
-                probeInfo.progress(100 * ((TileGrinder) tile).progress()/((TileGrinder) tile).MAX_PROGRESS(), 100, new ProgressStyleTOPAddonGrey().filledColor(0xff111111).alternateFilledColor(0xff222222).suffix("%").prefix("Grinding: "));
-            }
-
             if (tile instanceof TileTreeFarm) {
-                if (!((TileTreeFarm) tile).hasSaplings()) {
-                    probeInfo.text(TextFormatting.RED + "Out of saplings");
-                }
-
-                ItemStack axe = ((TileTreeFarm) tile).inventoryContents().get(((TileTreeFarm) tile).AXE_SLOT());
+                ItemStack axe = ((TileTreeFarm) tile).inventoryContents.get(TileTreeFarm.AXE_SLOT);
                 if (axe != null) {
                     textPrefixed(probeInfo, "Durability", axe.getMaxDamage() - axe.getItemDamage() + "/" + axe.getMaxDamage());
                 }
 
                 if (mode == ProbeMode.EXTENDED) {
-                    textPrefixed(probeInfo, "Range", ((TileTreeFarm) tile).RANGE() + " blocks");
+                    int range = 7 + 2 * ((TileTreeFarm) tile).getModifierForCategory(IUpgradeItem.ENUM_UPGRADE_CATEGORY.MEMORY);
+                    textPrefixed(probeInfo, "Range", String.format("%dx%d", range, range));
                 }
-
-
             }
 
             if (tile instanceof MachineGenerator) {
-                    textPrefixed(probeInfo, "Generating", (((MachineGenerator) tile).didWork() ? ((MachineGenerator) tile).getEnergyProduced() : 0) + " RF/t");
-            }
-        }
-
-        if (blockState.getBlock() instanceof BlockBasePressurePlate) {
-            TileEntity belowTile = world.getTileEntity(data.getPos().down());
-            if (belowTile != null && belowTile instanceof TileGrinder) {
-                probeInfo.progress(100 * ((TileGrinder) belowTile).progress()/((TileGrinder) belowTile).MAX_PROGRESS(), 100, new ProgressStyleTOPAddonGrey().filledColor(0xff111111).alternateFilledColor(0xff222222).suffix("%").prefix("Grinding: "));
+                textPrefixed(probeInfo, "Generating", ((MachineGenerator) tile).getEnergyProduced() + " RF/t");
             }
         }
     }
