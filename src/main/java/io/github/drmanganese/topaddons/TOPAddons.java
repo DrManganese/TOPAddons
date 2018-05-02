5b2055b5073343b569e3226c0953ec1ca8fd58c4
DrManganese <music.mathias@gmail.com>
2018-05-02 14:18:57 +0200

    Initial rework commit

---
+++ src/main/java/io/github/drmanganese/topaddons/TOPAddons.java
@@ -0,0 +1,22 @@
+package io.github.drmanganese.topaddons;
+
+import io.github.drmanganese.topaddons.reference.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+public class TOPAddons {
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/io/github/drmanganese/topaddons/reference/Reference.java
@@ -0,0 +1,8 @@
+package io.github.drmanganese.topaddons.reference;
+
+public final class Reference {
+
+    public static final String MOD_ID = "topaddons";
+    public static final String MOD_NAME = "TOP Addons";
+    public static final String MOD_VERSION = "@VERSION@";
+}
