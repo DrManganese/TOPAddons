8789da3dc09ad7dfc873913f97851e25eb82b359
DrManganese <DrManganese@users.noreply.github.com>
2021-03-08 22:18:14 +0100

    Forge: Add Fluid Tank mod to default gauge blacklist

--- src/main/java/io/github/drmanganese/topaddons/addons/forge/ForgeAddon.java
+++ src/main/java/io/github/drmanganese/topaddons/addons/forge/ForgeAddon.java
@@ -19,7 +19,7 @@ import com.google.common.collect.Lists;
 import mcjty.theoneprobe.api.ITheOneProbe;
 
 import javax.annotation.Nonnull;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class ForgeAddon extends TopAddon implements IAddonBlocks, IAddonElements, IAddonConfig, IAddonConfigProviders {
@@ -81,7 +81,7 @@ public class ForgeAddon extends TopAddon implements IAddonBlocks, IAddonElements
         }
 
         if (type == ModConfig.Type.COMMON)
-            gaugeModBlacklist = builder.comment("List of mod IDs for which no TOP Addons fluid gauge should be shown").define("gaugeModBlacklist", Collections.singletonList("mekanism"));
+            gaugeModBlacklist = builder.comment("List of mod IDs for which no TOP Addons fluid gauge should be shown").define("gaugeModBlacklist", Arrays.asList("mekanism", "fluidtank"));
         builder.pop();
     }
 
