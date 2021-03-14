be88c1e807a4bd47b0e46b6d26e8b781daa7d1dc
DrManganese <DrManganese@users.noreply.github.com>
2021-03-15 00:14:35 +0100

    Thermal Expansion: Ported addon to 1.16

---
+++ src/main/java/io/github/drmanganese/topaddons/addons/thermal/ThermalExpansionAddon.java
@@ -0,0 +1,60 @@
+package io.github.drmanganese.topaddons.addons.thermal;
+
+import io.github.drmanganese.topaddons.addons.TopAddon;
+import io.github.drmanganese.topaddons.addons.thermal.tiles.DynamoTileInfo;
+import io.github.drmanganese.topaddons.addons.thermal.tiles.EnergyCellTileInfo;
+import io.github.drmanganese.topaddons.addons.thermal.tiles.MachineTileInfo;
+import io.github.drmanganese.topaddons.addons.thermal.tiles.RedstoneControlTileInfo;
+import io.github.drmanganese.topaddons.api.IAddonBlocks;
+import io.github.drmanganese.topaddons.api.IAddonConfig;
+import io.github.drmanganese.topaddons.api.ITileInfo;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.config.ModConfig;
+
+import cofh.thermal.core.tileentity.DynamoTileBase;
+import cofh.thermal.core.tileentity.MachineTileProcess;
+import cofh.thermal.core.tileentity.ThermalTileBase;
+import cofh.thermal.core.tileentity.storage.EnergyCellTile;
+import com.google.common.collect.ImmutableMultimap;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+public class ThermalExpansionAddon extends TopAddon implements IAddonBlocks, IAddonConfig {
+
+    private static final ImmutableMultimap<Class<? extends TileEntity>, ITileInfo> TILES = ImmutableMultimap.of(
+        DynamoTileBase.class, new DynamoTileInfo(),
+        EnergyCellTile.class, new EnergyCellTileInfo(),
+        MachineTileProcess.class, new MachineTileInfo(),
+        ThermalTileBase.class, new RedstoneControlTileInfo()
+    );
+
+    public static ForgeConfigSpec.BooleanValue alwaysShowCellIo;
+
+    public ThermalExpansionAddon() {
+        super("thermal_expansion");
+    }
+
+    @Nonnull
+    @Override
+    public ImmutableMultimap<Class<? extends TileEntity>, ITileInfo> getTileInfos() {
+        return TILES;
+    }
+
+    @Override
+    public void buildConfig(ForgeConfigSpec.Builder builder, ModConfig.Type type) {
+        if (type == ModConfig.Type.CLIENT) {
+            builder.push("thermal");
+            alwaysShowCellIo = builder.comment("Always show Flux Cell input/output rate (even when not extended)").define("alwaysShowCellIo", false);
+            builder.pop();
+        }
+    }
+
+    @Override
+    public List<ForgeConfigSpec.ConfigValue<?>> getClientConfigValuesToSync() {
+        return Collections.singletonList(alwaysShowCellIo);
+    }
+}
---
+++ src/main/java/io/github/drmanganese/topaddons/addons/thermal/tiles/DynamoTileInfo.java
@@ -0,0 +1,24 @@
+package io.github.drmanganese.topaddons.addons.thermal.tiles;
+
+import io.github.drmanganese.topaddons.api.ITileInfo;
+import io.github.drmanganese.topaddons.util.InfoHelper;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+import cofh.thermal.core.tileentity.DynamoTileBase;
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.ProbeMode;
+
+import javax.annotation.Nonnull;
+
+public class DynamoTileInfo implements ITileInfo<DynamoTileBase> {
+
+    @Override
+    public void addProbeInfo(ProbeMode probeMode, IProbeInfo probeInfo, PlayerEntity player, World world, BlockState blockState, IProbeHitData hitData, @Nonnull DynamoTileBase tile) {
+        if (tile.isActive)
+            InfoHelper.textPrefixed(probeInfo, "{*topaddons:generating*}", String.format("%d RF/t", tile.getCurSpeed()));
+    }
+}
