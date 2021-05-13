4d0bb0a1d6b17e01a2daa019ea7d844b012d6734
DrManganese <DrManganese@users.noreply.github.com>
2021-05-13 10:12:44 +0200

    Vanilla: Removed Note Block config option

--- src/main/java/io/github/drmanganese/topaddons/addons/vanilla/VanillaAddon.java
+++ src/main/java/io/github/drmanganese/topaddons/addons/vanilla/VanillaAddon.java
@@ -16,15 +16,13 @@ import net.minecraft.tileentity.AbstractFurnaceTileEntity;
 import net.minecraft.tileentity.BeaconTileEntity;
 import net.minecraft.tileentity.BeehiveTileEntity;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.config.ModConfig;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 
 import javax.annotation.Nonnull;
 
-public class VanillaAddon extends TopAddon implements IAddonBlocks, IAddonEntities, IAddonConfig {
+public class VanillaAddon extends TopAddon implements IAddonBlocks, IAddonEntities {
 
     private static final ImmutableMap<Block, IBlockInfo> BLOCKS = ImmutableMap.of(
         Blocks.COMPOSTER, new ComposterInfo(),
@@ -37,8 +35,6 @@ public class VanillaAddon extends TopAddon implements IAddonBlocks, IAddonEntiti
         BeaconTileEntity.class, new BeaconInfo(),
         BeehiveTileEntity.class, BeehiveInfo.INSTANCE
     );
-
-    public static ForgeConfigSpec.BooleanValue showPitch;
 
     public VanillaAddon() {
         super("vanilla");
@@ -60,14 +56,5 @@ public class VanillaAddon extends TopAddon implements IAddonBlocks, IAddonEntiti
     @Override
     public ImmutableMap<Block, IBlockInfo> getBlockInfos() {
         return BLOCKS;
-    }
-
-    @Override
-    public void buildConfig(final ForgeConfigSpec.Builder builder, final ModConfig.Type type) {
-        if (type == ModConfig.Type.COMMON) {
-            builder.push(name);
-            showPitch = builder.comment("Display Note Block pitch and instrument").define("showPitch", true);
-            builder.pop();
-        }
     }
 }
